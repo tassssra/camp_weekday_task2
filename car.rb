cd7aab99a381980ce40932461c2fc4d3e3ddf3db
yasuda <tasssra@gmail.com>
2019-03-05 12:27:59 +0900

    平日課題2-3 完了

--- car.rb
+++ car.rb
@@ -2,30 +2,33 @@ class Car
   # 定数を追加
   UP_SPEED = 10
   DOWN_SPEED = 20
+  # 定数 MAX_PASSENGERS を追加 (課題2-3)
+  MAX_PASSENGERS = 4
 
   @@count = 0
-  attr_accessor :number, :color
+  attr_accessor :number, :color, :passengers # :passengersを追加 (課題2-3)
   # speedは外部から設定しないのでreaderで定義
   # @speedが使えるようになる
   attr_reader :speed
 
-  def initialize(number, color)
+  def initialize(number, color) # initializeメソッドにpassengersを追加
     @number = number
     @color = color
     # @speedを初期化
     @speed = 0
+    @passengers = 1 # Carクラスのインスタンス生成時に、passengersに1を追加 (課題2-3)
     @@count += 1
   end
 
   # 加速用のメソッド追加
   def speed_up
     @speed += UP_SPEED
-    alert = "[ALERT]" # [ALERT]部分の変更に備えて変数alertを代入
+    alert = "[ALERT]" # [ALERT]部分の変更に備えて定数alertに代入 (課題2-1)
 
-    # 30以上で警告。50以上で警告の上、20減速するif文を追加。
+    # 30以上でアラート表示。50以上でアラート表示の上、20減速するif文を追加。 (課題2-1)
     if @speed >= 50
       puts "#{alert}スピードが#{@speed}になりました。減速します。"
-      @speed -= 20
+      @speed -= DOWN_SPEED # 定数 DOWN_SPEEDは数値20 (Carクラスで定義)
     elsif @speed >= 30
       puts "#{alert}スピードが#{@speed}になりました。"
     end
@@ -45,9 +48,21 @@ class Car
   def self.count
     @@count  # return @@countの略
   end
+
+  # get_onメソッドを追加。4人以上になると乗車できなくなる仕様。 (課題2-3)
+  def get_on
+    if @passengers >= MAX_PASSENGERS # MAX_PASSENGERS == 4
+      puts "乗車できません。この車の最大乗車人数は4人です。"
+    else
+      @passengers += 1 # passengersに1を追加
+      puts "乗車しました。"
+    end
+  end
+
 end
 
 
+# Carクラスを継承したTrackCarクラスを追加 (課題2-2)
 class TrackCar < Car
   attr_reader :load_weight
 
